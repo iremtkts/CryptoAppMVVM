93e849e856c8935ce8ae80e21e238d059fdf5f5f
IremTektas <tektasirem13@gmail.com>
2023-03-22 23:24:47 +0300

    mvvm folders created , webservice created

---
+++ CryptoApp/Model/CryptoCurrency.swift
@@ -0,0 +1,15 @@
+//
+//  CryptoCurrency.swift
+//  CryptoApp
+//
+//  Created by iremt on 22.03.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency : Decodable{
+    
+    let currency : String
+    let price : String
+    
+}
---
+++ CryptoApp/Service/WebService.swift
@@ -0,0 +1,30 @@
+//
+//  WebService.swift
+//  CryptoApp
+//
+//  Created by iremt on 22.03.2023.
+//
+
+import Foundation
+
+
+class WebService {
+    
+    func downloadCurrencies(url : URL , completion : @escaping([CryptoCurrency]?) ->()) {
+        // Cryptocurrency bir dizi içinde gelecek optional olmalı void döndürcek
+        
+        URLSession.shared.dataTask(with: url) { data , response , error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                
+                if let cryptoList = cryptoList {
+                    completion(cryptoList)
+                }
+            }
+        }
+    }
+}
