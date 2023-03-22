8c4b0b78d0e72e5af766bc0acc5e692a5c687c45
IremTektas <tektasirem13@gmail.com>
2023-03-22 23:03:43 +0300

    user interface designed

---
+++ CryptoApp/CryptoTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CryptoTableViewCell.swift
+//  CryptoApp
+//
+//  Created by iremt on 22.03.2023.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var currencyText: UILabel!
+    
+    
+    @IBOutlet weak var priceText: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
